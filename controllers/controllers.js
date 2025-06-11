58247ba83636d03fb546ec3fa9a8b2a6099e2a96
hinds1ght <markgil_yu@yahoo.com>
2025-06-11 09:44:23 +0800

    connect controller to routes

---
+++ controllers/controllers.js
@@ -0,0 +1,22 @@
+
+//const asyncHandler = require('express-async-handler');
+//const bcrypt = require('bcryptjs');
+
+
+//REGISTER*******************
+
+exports.register = (req, res ) => {
+    res.send('register')   
+};
+
+// LOGIN**********************
+
+exports.login = (req, res)=>{
+    res.send('login')
+}
+
+// POSTS*******************
+
+exports.posts = (req, res)=>{
+    res.send('posts')
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const user = require('../controllers/controllers')
 const route = express.Router();
 
-route.post('/register', (req, res)=>res.send('register'))
-route.post('/login', (req, res)=>res.send('login'))
-route.get('/posts', (req, res)=>res.send('posts'))
+route.post('/register', user.register)
+route.post('/login', user.login)
+route.get('/posts', user.posts)
 
 module.exports = route;
