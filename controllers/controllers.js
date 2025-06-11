a49aaf9f0d6c2f1bcd1a8dc77e29a609f08933cb
hinds1ght <markgil_yu@yahoo.com>
2025-06-11 15:49:25 +0800

    add register post func, bcrypt hashing

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,13 +1,30 @@
 
-//const asyncHandler = require('express-async-handler');
-//const bcrypt = require('bcryptjs');
-
+const prisma = require('../model/model');
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 
 //REGISTER*******************
 
-exports.register = (req, res ) => {
-    res.send('register')   
-};
+exports.register = asyncHandler(async (req, res, next) => {
+        const { email, password } = req.body;
+
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+
+        if (existingUser) {
+            res.status(400);
+            throw new Error("User already exists");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+            }
+        });
+            res.json(newUser)
+});
 
 // LOGIN**********************
 
