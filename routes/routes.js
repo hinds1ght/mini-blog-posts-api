a86f835d55964d3483e8e1a0e0379852ba7fb882
hinds1ght <markgil_yu@yahoo.com>
2025-06-12 09:09:31 +0800

    add verify middleware to post route

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const user = require('../controllers/controllers')
+const user = require('../controllers/controllers');
+const verifyToken = require('../auth/verifyToken');
 const route = express.Router();
 
 route.post('/register', user.register)
 route.post('/login', user.login)
-route.get('/posts', user.posts)
+route.get('/posts', verifyToken , user.posts)
 
 module.exports = route;
